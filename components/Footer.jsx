71bbb5a4b75d6f85cc83d19e2d25e0086044f03b
Kathleen West <kathleen.elizabeth.west@gmail.com>
2025-03-10 16:22:43 -0400

    Updates

--- components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <hr />
-            <p>Created by <a href="https://portfolio.katiegirl.net/">Kathleen West</a></p>
-        </footer>
-        );
-}
